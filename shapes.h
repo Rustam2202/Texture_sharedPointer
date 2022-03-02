76db6c7d4dc859122b96dc69352001c2988d9ae9
Рустам Нурмиев <Rustam2202@yandex.ru>
2022-03-02 18:59:37 +0300

    intermed save

--- shapes.h
+++ shapes.h
@@ -12,7 +12,12 @@ class Shape {
 public:
 	// Фигура после создания имеет нулевые координаты и размер,
 	// а также не имеет текстуры
-	explicit Shape(ShapeType type) : type_(type) {}
+	explicit Shape(ShapeType type) : type_(type) {
+		point_.x = 0;
+		point_.y = 0;
+		size_.height = 0;
+		size_.width = 0;
+	}
 
 	void SetPosition(Point pos) {
 		point_ = pos;
@@ -35,12 +40,18 @@ public:
 		//(void)image;
 		// Заглушка. Реализуйте метод самостоятельно
 
-
-		for (string x_axis : image) {
-			for (char ch : x_axis) {
-				Point p(ch, x_axis);
-				if (IsPointInEllipse(p, size_)) {
-
+		for (int y = point_.y, y_t = 0; y < size_.height || y_t < texture_.get()->GetSize().height; ++y, ++y_t) {
+			for (int x = point_.x, x_t = 0; x < size_.width||x_t<texture_.get()->GetSize().width; ++x, ++x_t) {
+				if (type_ == ShapeType::ELLIPSE) {
+					if (IsPointInEllipse({ x,y }, size_)) {
+						image[y][x] = ' ';
+					}
+					else {
+						image[y][x] = texture_.get()->GetPixelColor({ y_t, x_t });
+					}
+				}
+				else {
+					image[y][x] = texture_.get()->GetPixelColor({ y_t, x_t });
 				}
 			}
 		}
